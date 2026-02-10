aef6f37eaf9027c0b7fb2446670f9b14d9b8b72e
lxt <myuuz@outlook.com>
2026-02-10 17:27:04 +0800

    添加一些关于uci的使用和不同表单的学习, 并内置一个myapp配置文件用于测试

--- package/luci/luci-mod-test/htdocs/luci-static/resources/view/test_view/uci_page_01.js
+++ package/luci/luci-mod-test/htdocs/luci-static/resources/view/test_view/uci_page_01.js
@@ -1,23 +1,65 @@
 'use strict';
 'require view';
-'require form';
 'require uci';
 
-return view.extend({
+/*
+config global 'settings'
+    option enable '1'
+    option mode 'advanced'
 
-    load: function () {
-        return uci.load('system'); //uci show system
+config profile
+    option name 'profile1'
+    option ip '10.0.0.1'
+    list service 'http'
+    list service 'ssh'
+
+config profile
+    option name 'profile2'
+    option ip '10.0.0.2'
+    list service 'dns'
+*/
+
+/*
+root@OpenWrt:~# uci show myapp
+
+myapp.settings=global
+myapp.settings.enable='1'
+myapp.settings.mode='advanced'
+myapp.@profile[0]=profile
+myapp.@profile[0].name='profile1'
+myapp.@profile[0].ip='10.0.0.1'
+myapp.@profile[0].service='http' 'ssh'
+myapp.@profile[1]=profile
+myapp.@profile[1].name='profile2'
+myapp.@profile[1].ip='10.0.0.2'
+myapp.@profile[1].service='dns'
+*/
+
+return view.extend({
+    load: function() {
+        return uci.load('myapp'); //先load,才能get. uci.load必须在load函数中完成不能去render中
     },
 
-    render: function () {
+    render: function() {
+        //没有重名的直接取值
+        var enable = uci.get('myapp', 'settings', 'enable');
+        var mode   = uci.get('myapp', 'settings', 'mode');
+        
+        //数组形式的必须使用 sections 取出来
+        var profiles = uci.sections('myapp', 'profile');
+        console.log(profiles.length  )
+        if(profiles.length > 0) {
+          for(var i = 0; i < profiles.length  ; i++) {
+            console.log(profiles[i].ip);
+          }
+        }
 
-        var m = new form.Map('system', _('System'));
+        //数组取出方式2
+        var s = uci.sections('myapp', 'profile')[0];
 
-        var s = m.section(form.TypedSection, 'system');
-        s.anonymous = true;
-
-        var o = s.option(form.Value, 'hostname', _('Hostname'));
-
-        return m.render();
+        return E('div', {}, [
+            E('p', {id: 'p1',}, 'Enable: ' + enable),
+            E('p', {'data-mode': 3}, 'Mode: ' + mode),
+          ]);
     }
 });
--- package/luci/luci-mod-test/htdocs/luci-static/resources/view/test_view/uci_page_02.js
+++ package/luci/luci-mod-test/htdocs/luci-static/resources/view/test_view/uci_page_02.js
@@ -1,77 +1,79 @@
 'use strict';
 'require view';
-'require dom';
-'require ui';
+'require uci';
 'require form';
 
+/*
+config global 'settings'
+    option enable '1'
+    option mode 'advanced'
 
-var data = {
-	value_01: {
-		name: ""
-	},
-	value_02: {
-		value_02_01: "",
-		value_02_02: "",
-		enabled: 0,
-		protocol: "",
-		dns_servers: [],
-		custom_script: "",
-		apply_btn: "a?"
-	}
-};
+config profile
+    option name 'profile1'
+    option ip '10.0.0.1'
+    list service 'http'
+    list service 'ssh'
+
+config profile
+    option name 'profile2'
+    option ip '10.0.0.2'
+    list service 'dns'
+*/
+
+/*
+root@OpenWrt:~# uci show myapp
+
+myapp.settings=global
+myapp.settings.enable='1'
+myapp.settings.mode='advanced'
+myapp.@profile[0]=profile
+myapp.@profile[0].name='profile1'
+myapp.@profile[0].ip='10.0.0.1'
+myapp.@profile[0].service='http' 'ssh'
+myapp.@profile[1]=profile
+myapp.@profile[1].name='profile2'
+myapp.@profile[1].ip='10.0.0.2'
+myapp.@profile[1].service='dns'
+*/
 
 
 return view.extend({
+    load: function() {
+        return uci.load('myapp');
+    },
 
-	render: function(lanIP) {
-	    // 假设 data 是前端需要渲染的 JSON 数据
-	    data.value_01.name = lanIP;
+    render: function() {
+        var m,s,o;
 
-	    // 创建一个 JSONMap 对象
-	    // 参数：
-	    // data         -> 绑定的数据源，通常是后端返回的 JSON
-	    // "test_page_001" -> 页面 ID，唯一标识该表单页面
-	    // "表单标题"       -> 页面标题显示在顶部，可带描述性文字
-	    var m = new form.JSONMap(
-	        data,
-	        "test_page_001",
-	        "这个应该是表单标题和下面内容的线之间的内容"
-	    );
+        //form.Map 直接绑定uci内容, form.JSONMap 绑定的是 js 中的数据结构
+        m = new form.Map('myapp', '这些是myapp相关内容');
+        s = m.section(form.NamedSection, 'settings', 'setting内容');
+        o = s.option(form.Flag, 'enable', 'enable属性');
+        //如果不设置该属性, 那么取消勾选提交后就会删除该属性,这个时候后端判断的写法就需要:
+        // [ -n "$(uci get xxx.enable 2>/dev/null)" ] && enable
+        o.rmempty = false;   
+        
+        o = s.option(form.DummyValue, 'mode', 'mode属性');
 
-	    var s, o;
+        //效果图 https://raw.githubusercontent.com/LOVEUUZ/res/refs/heads/main/openwrt/openwrt%E8%87%AA%E5%AE%9A%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6%E6%95%B0%E7%BB%84%E5%9E%8B%E5%86%85%E5%AE%B9_TypedSection%20.png
+        s = m.section(form.TypedSection , 'profile', 'profile列表');
+        o = s.option(form.Value, 'name', 'name属性');
+        o = s.option(form.Value, 'ip', 'ip属性');
+        o = s.option(form.DynamicList, 'service', 'service项目');
 
-	    // =====================
-	    // NamedSection 示例
-	    // =====================
-	    // NamedSection 参数：
-	    // form.NamedSection -> Section 类型
-	    // "value_01"        -> Section 名称，对应 JSON 或 UCI 的 section 名
-	    // null              -> 如果有类型，可以填写类型名称，null 表示不指定
-	    // "测试分组"        -> Section 的标题显示在前端
-	    s = m.section(form.NamedSection, "value_01", null, "测试分组");
+        //效果图 https://raw.githubusercontent.com/LOVEUUZ/res/refs/heads/main/openwrt/openwrt%E8%87%AA%E5%AE%9A%E9%85%8D%E7%BD%AE%E6%96%87%E4%BB%B6%E6%95%B0%E7%BB%84%E5%9E%8B%E5%86%85%E5%AE%B9_GridSection.png
+        s = m.section(form.GridSection , 'profile', 'profile列表');
+        o = s.option(form.Value, 'name', 'name属性');
+        o = s.option(form.Value, 'ip', 'ip属性');
+        // o = s.option(form.DynamicList, 'service', 'service项目'); //GridSection不支持DynamicList
 
-	    // Value 输入框示例
-	    // 参数：
-	    // form.Value  -> 组件类型
-	    // "name"      -> 绑定字段名，对应 data.value_01.name
-	    // "LAN IP"    -> 前端显示标题
-	    o = s.option(form.Value, "name", "LAN IP");
-	    o.datatype = "ip4addr";           // 字段类型，LuCI 内置
-	    o.placeholder = "请输入 LAN IP";   // 输入框提示文字
-	    o.rmempty = false;                // 是否允许为空
-	    o.default = "0.0.0.0";         		// 默认值
+        //效果图 https://raw.githubusercontent.com/LOVEUUZ/res/refs/heads/main/openwrt/openwrt_%E6%95%B0%E7%BB%84%E5%9E%8B%E5%92%8Clist%E5%9E%8B%E5%86%85%E5%AE%B9%E7%BB%84%E5%90%88_TableSection.png
+        s = m.section(form.TableSection , 'profile', 'profile列表');
+        o = s.option(form.Value, 'name', 'name属性');
+        o = s.option(form.Value, 'ip', 'ip属性');
+        o = s.option(form.DynamicList, 'service', 'service项目');
 
-	    // 最后渲染 Map 并返回 HTML
-	    return m.render();
-	},
-
-
-	handleSave: function () {
-		ui.addNotification(null,
-			E('p',
-				'Hello 1 ' + data.value_01.name +
-				"\n2: " + data.value_02.value_02_01
-			)
-		);
-	}
+        
+        return m.render();
+    }
 });
